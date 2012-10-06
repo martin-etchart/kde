c7125ea9b35bbe703c88ac4c2cee2c1b04a9326b
Juan Cardelino <juan.cardelino@gmail.com>
2012-10-06 14:36:39 -0300

    polished dct comparison between matlab, fftw, and gsl

--- matlab/test_dct.m
+++ matlab/test_dct.m
@@ -4,19 +4,24 @@ home
 %% make matlab give the same output as fftw
 %input
 x=[2 1 0]
-
+n=length(x);
 %% regular matlab output
+% idct
 idct_matlab=idct(x)
-
-%% regular dct
+% dct
 dct_matlab=dct(idct_matlab)
 
 %% fftw dct
 %normalized input
 y = x;
 y(1) = x(1) / sqrt(2);
-idct_fftw= idct(y) *sqrt(2*length(x))
+idct_fftw= idct(y) *sqrt(2*n)
 
-dct_fftw=dct(idct_fftw/sqrt(2*length(x)));
+xt=idct_fftw/sqrt(2*n)
+dct_fftw=dct(xt);
 dct_fftw(1)=dct_fftw(1)*sqrt(2);
 dct_fftw
+
+%% test direct DCT
+in=[1.8618    1.1547    0.4476]
+out_matlab=dct(in)
--- matlab/test_kde_cdt.m
+++ matlab/test_kde_cdt.m
@@ -1,8 +1,19 @@
 N=length(initial_data);
-y1=dct(initial_data);
-y2=kde_dct1d(initial_data)/sqrt(N);
-figure
+y1=dct(initial_data*sqrt(N));
+y2=kde_dct1d(initial_data);
+figure(1)
 plot(y1,'-r')
 hold on
 plot(y2,'-b')
 hold off
+
+%%
+dct_error.y=abs(y1-y2)./abs(y1);
+dct_error.mean=mean(dct_error.y);
+
+dct_error.max=max(dct_error.y);
+
+dct_error
+
+figure(2)
+plot(dct_error.y)
